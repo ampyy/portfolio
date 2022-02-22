b763648838afa0055577af7b04e95229aa49648e
ampyy <pandeyaman2485@gmail.com>
2022-02-22 22:36:04 +0530

    changes

--- ampy/settings.py
+++ ampy/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-eue@7+!&$ncxy=ei-!esvn%r%)bsp7=8f9s3bw=3#8%ps%61v#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['amanpandey.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['amanpandey.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -66,15 +66,27 @@ WSGI_APPLICATION = 'ampy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dph5336n7vl0q',
+        'USER': 'iwvmolnrmotepj',
+        'PASSWORD': '002971c8a066c85bfe59a7bf37cf43026bbafb7f269c90ce46c6f46e2f992e19',
+        'HOST': 'ec2-3-230-238-86.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
