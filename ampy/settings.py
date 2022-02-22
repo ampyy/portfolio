e4b0255beaae85d6280c136cb2eb301d7fc2c9a2
ampyy <pandeyaman2485@gmail.com>
2022-02-23 00:43:07 +0530

    changes

--- ampy/settings.py
+++ ampy/settings.py
@@ -66,25 +66,25 @@ WSGI_APPLICATION = 'ampy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd48rkot8slcv6e',
-        'USER': 'vozrrfconqxazc',
-        'PASSWORD': '780fbb82eb5d1fb2cc13f81fac6a1c3eb561f4d17251fae1ad4a43bba527b14e',
-        'HOST': 'ec2-34-203-91-150.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'd48rkot8slcv6e',
+#         'USER': 'vozrrfconqxazc',
+#         'PASSWORD': '780fbb82eb5d1fb2cc13f81fac6a1c3eb561f4d17251fae1ad4a43bba527b14e',
+#         'HOST': 'ec2-34-203-91-150.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
